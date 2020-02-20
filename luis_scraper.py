9ed4d8cdcd5e9403e8861b52f404b2879f9dcc39
Robert Young <robert.young.ie@gmail.com>
2020-02-20 14:13:31 +0000

    feat: Start of scraper code, updated gitignore

---
+++ luis_scraper.py
@@ -0,0 +1,33 @@
+import time
+import config
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+option = webdriver.ChromeOptions()
+option.add_argument(" - incognito")
+
+browser = webdriver.Chrome(executable_path='~/chromedriver', chrome_options=option)
+
+
+def login_luis():
+    browser.get("https://eu.luis.ai")
+    time.sleep(3)
+    browser.find_element_by_link_text('Sign in').click()
+    time.sleep(3)
+    browser.find_element_by_id('i0116').send_keys(config.username)
+    browser.find_element_by_id('idSIButton9').click()
+    time.sleep(3)
+    browser.find_element_by_id('i0118').send_keys(config.password)
+    browser.find_element_by_id('idSIButton9').click()
+    time.sleep(5)
+    ActionChains(browser).send_keys(Keys.ESCAPE).perform()
+    browser.find_element_by_link_text('myapp_v01').click()
+    time.sleep(5)
+
+
+login_luis()
