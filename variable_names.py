8fdf5c13ec77b190c2d48b2c57219f20dad6463e
Robert Young <robert.young.ie@gmail.com>
2020-02-23 21:23:59 +0000

    chore: Added intent titles to be used when retrieving results from Luis

--- variable_names.py
+++ variable_names.py
@@ -3,3 +3,6 @@ list_of_headers = ['Intent', 'Model Version', 'Commments', 'Size',	'Result', 'Ar
                    'None', 'Preferences', 'Question', 'Restart', 'command', 'end_conversation', 'greeting', 'learning',
                    'suggestion', 'language', 'No',	'Subtopics 1',	'Topics', 'Yes', 'TP', 'FP', 'FN', 'TN',
                    'Precision', 'Recall', 'F-Score']
+
+intent_titles = ['ChangeLanguage', 'command', 'Confirmation', 'Delete_and_Restart', 'end_conversation', 'greeting',
+                 'How_are_you', 'learning_corrected', 'learning', 'None', 'Preferences', 'Restart', 'suggestion']
