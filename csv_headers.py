5d119965b27c41100c65e4050e546a8922d01a9c
Robert Young <robert.young.ie@gmail.com>
2020-02-23 21:05:51 +0000

    chore: Added headers for csv output to a separate python file
    Added to a separate file due to the length of the list

---
+++ csv_headers.py
@@ -0,0 +1,5 @@
+list_of_headers = ['Intent', 'Model Version', 'Commments', 'Size',	'Result', 'Arabic', 'ChangeLanguage', 'Complete',
+                   'Confirmation',	'Delete_and_Restart', 'Gibberish', 'HowAreYouResponse', 'How_are_you', 'Ignore',
+                   'None', 'Preferences', 'Question', 'Restart', 'command', 'end_conversation', 'greeting', 'learning',
+                   'suggestion', 'language', 'No',	'Subtopics 1',	'Topics', 'Yes', 'TP', 'FP', 'FN', 'TN',
+                   'Precision', 'Recall', 'F-Score']
