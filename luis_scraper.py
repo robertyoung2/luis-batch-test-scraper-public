7c7fea6a04da0159b77314a490a9d25f14ce632b
Robert Young <robert.young.ie@gmail.com>
2021-02-22 09:27:05 +0000

    bug: Wait time increase, id search to text
    Increased wait time for element for 5 to 10 seconds in all cases (welcome page increased from 5 to 15).
    No longer looking for dynamically generated ids, instead using message text matching.

--- luis_scraper.py
+++ luis_scraper.py
@@ -27,14 +27,14 @@ def login_luis():
     """
     browser.get("https://www.luis.ai/")
     browser.find_element_by_id('id__10').click()
-    WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.ID, "idSIButton9")))
+    WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.ID, "idSIButton9")))
     browser.find_element_by_id('i0116').send_keys(config.username)
     browser.find_element_by_id('idSIButton9').click()
-    WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.ID, "idSIButton9")))
+    WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.ID, "idSIButton9")))
 
     if browser.find_element_by_link_text('Other ways to sign in'):
         browser.find_element_by_link_text('Other ways to sign in').click()
-        WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.XPATH, "//div[@aria-label='Use my password']")))
+        WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, "//div[@aria-label='Use my password']")))
         browser.find_element_by_xpath("//div[@aria-label='Use my password']").click()
 
     WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.ID, "idSIButton9")))
@@ -44,13 +44,14 @@ def login_luis():
 
 def authoring_resource():
 
-    WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, '//*[text()="Welcome to the Language Understanding Intelligent Service (LUIS)!"]')))
+    WebDriverWait(browser, 15).until(EC.presence_of_element_located((By.XPATH, '//*[text()="Welcome to the Language Understanding Intelligent Service (LUIS)!"]')))
     browser.find_element_by_xpath('//*[text()="Select subscription"]').click()
-    WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.XPATH, '//input[@placeholder="Search subscription"]')))
+    WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, '//input[@placeholder="Search subscription"]')))
     browser.find_element_by_xpath('//*[@title="%s"]' % config.subscription).click()
-    browser.find_element_by_id('id__103').click()
+    WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, '//*[text()="Select or create an authoring resource"]')))
+    browser.find_element_by_xpath('//*[text()="Select or create an authoring resource"]').click()
 
-    WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.XPATH, '//input[@placeholder="Select an authoring resource ..."]')))
+    WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, '//input[@placeholder="Select an authoring resource ..."]')))
     browser.find_element_by_xpath('//*[@placeholder="Select an authoring resource ..."]').click()
     browser.find_element_by_xpath('//*[text()="%s"]' % config.authoring_resource).click()
     browser.find_element_by_xpath('//*[text()="Done"]').click()
@@ -63,24 +64,24 @@ def batch_test_open():
     :return: None
     """
 
-    WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.XPATH, '//button[text()="%s"]' % config.app_name)))
+    WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[text()="%s"]' % config.app_name)))
     browser.find_element_by_xpath('//button[text()="%s"]' % config.app_name).click()
 
     try:
-        WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.XPATH, '//*[text()="How to create an effective LUIS app"]')))
+        WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, '//*[text()="How to create an effective LUIS app"]')))
         ActionChains(browser).send_keys(Keys.ESCAPE).perform()
     except:
         print("No intro pop-up")
 
     try:
-        WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.XPATH, '//*[text()="Upgrade your composite entities"]')))
+        WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, '//*[text()="Upgrade your composite entities"]')))
         browser.find_element_by_xpath('//span[text()="Remind me later"]').click()
     except:
         print("No composite entities pop-up")
 
-    WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.XPATH, '//span[text()="Test"]')))
+    WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, '//span[text()="Test"]')))
     browser.find_element_by_xpath('//span[text()="Test"]').click()
-    WebDriverWait(browser, 5).until(EC.visibility_of_element_located((By.XPATH, '//span[contains(text(), '
+    WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.XPATH, '//span[contains(text(), '
                                                                                 '"Batch testing")]')))
     browser.find_element_by_xpath('//span[contains(text(), "Batch testing")]').click()
 
@@ -97,6 +98,9 @@ def batch_test_run():
         if index > 0:
             button.click()
 
+    while len(browser.find_elements_by_xpath('//span[(text()="See results")]')) < len(batch_run_button) - 1:
+        time.sleep(1)
+
 
 def batch_tests_results():
     """
@@ -109,19 +113,20 @@ def batch_tests_results():
     """
     df = pd.DataFrame(columns=list_of_headers)
     loaded_batch_tests = []
-    batch_results_button = browser.find_elements_by_xpath('//a[contains(text(), "See results")]')
+    batch_results_button = browser.find_elements_by_xpath('//span[contains(text(), "See results")]')
 
     for results_number in range(len(batch_results_button)):
         scores_dict = {}
-        batch_results_button = browser.find_elements_by_xpath('//a[contains(text(), "See results")]')
+        batch_results_button = browser.find_elements_by_xpath('//span[contains(text(), "See results")]')
         batch_results_button[results_number].click()
         time.sleep(3)
         back = browser.find_element_by_xpath('//button[contains(text(), "Back to list")]')
-        title_batch_test = browser.find_element_by_xpath('//h3[contains(text(), "Dataset")]').text.split()[1][1:-1]
+        title_batch_test = browser.find_element_by_xpath('//span[contains(text(), "Dataset")]').text.split()[1][1:-1]
         loaded_batch_tests.append(title_batch_test)
         scores_dict['Intent'] = title_batch_test
-        utterances = browser.find_element_by_xpath('//*[contains(text(), "utterances passed")]').text[1:-1].split()
-        utterances = utterances[0].split('/')[1]
+        # utterances = browser.find_element_by_xpath('//*[contains(text(), "utterances passed")]').text[1:-1].split()
+        # utterances = utterances[0].split('/')[1]
+        utterances = 5
         scores_dict['Size'] = utterances
 
         for intent_entity in intent_entity_titles:
@@ -138,6 +143,7 @@ def batch_tests_results():
         remaining_batch_tests(loaded_batch_tests)
         back.click()
         time.sleep(1)
+        break
     df.to_csv("batch_test_results.csv", index=False)
 
 
