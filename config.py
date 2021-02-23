3c305b4a247cf883bf8d260cecb1a7010e5c39b6
Robert Young <robert.young.ie@gmail.com>
2021-02-23 10:02:02 +0000

    feats: Two additional variables added

--- config.py
+++ config.py
@@ -2,3 +2,5 @@ username = 'billgates@microsoft.com'
 password = 'i-love-cortana'
 home_path = '~/chromedriver'
 app_name = 'myapp_v01'
+subscription = 'azure-subscription-dropdown'
+authoring_resource = "you-authoring-resource"
